ed7e3a671fd8b452cd33aba3f4213ad52e06a5db
Robin-1995 <60781999+Robin-1995@users.noreply.github.com>
2020-05-13 14:18:46 +0100

    Add files via upload

--- Collatz-Conjecture.py
+++ Collatz-Conjecture.py
@@ -3,22 +3,36 @@
 # the sequence will always converge to 1
 
 
-# this function returns the number of steps required to reach the value of 1
 def collatz_conjecture(n):
-    count = 0
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    i = 0
+    yax = []
 
     try:
-
         if n == 0:
             raise ValueError
         else:
-
             while n > 1:
+                yax.append(n)  # add the value of n to yax
+
                 if n % 2 == 0:
                     n = n/2
                 else:
                     n = 3*n+1
-                count += 1
-            return count
+                i += 1
+
+            # create a dataframe and plot n against i to show a graphically what the algorithm is doing
+            yax.append(1)
+            xax = [c for c in range(0, i+1)]
+            df = pd.DataFrame(yax, index=xax)
+            fig = df.plot.line(legend=False)
+            fig.set_xlabel("term")
+            fig.set_xlim((0, i))
+            fig.set_ylabel("n")
+            plt.show()
+
+            return i
+
     except (TypeError, ValueError):
         print("Error: n must be an integer greater than 0")
