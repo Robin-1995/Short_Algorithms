6ce552c078d55c6229f31426d067a425aced0fa4
Robin-1995 <robinplatts@live.co.uk>
2020-05-13 15:37:33 +0100

    Added sieve of erathosthenes to find all prime numbers up to n

--- prime_factors_of_n.py
+++ prime_factors_of_n.py
@@ -1,2 +1,16 @@
 def prime_factors(n):
-    pass
+
+    def sieve_of_eratosthenes(val):
+        nums = [True for x in range(2,val+1)]
+        primes = []
+
+        i = 2
+        while i<=n:
+            if nums[i] == True:
+                for j in range(i**2,val+1,i):
+                    nums[i] = False
+                i += 1
+        for num in enumerate(nums):
+            if nums[1] == True:
+                primes.append(nums[0])
+        return primes
