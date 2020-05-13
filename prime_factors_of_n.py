1dc726ab5120028bdc209bd4e3e2317def6dba7d
Robin-1995 <robinplatts@live.co.uk>
2020-05-13 16:15:35 +0100

    changed sieve of erat to not include even numbers

--- prime_factors_of_n.py
+++ prime_factors_of_n.py
@@ -1,16 +1,18 @@
 def prime_factors(n):
 
-    def sieve_of_eratosthenes(val):
-        nums = [True for x in range(2,val+1)]
-        primes = []
-
-        i = 2
+    def sieve_of_eratosthenes(n):
+        nums = [x for x in range(3,n+1) if x%2!=0]
+        primes = [2]
+        i = 0
         while i<=n:
-            if nums[i] == True:
-                for j in range(i**2,val+1,i):
-                    nums[i] = False
-                i += 1
-        for num in enumerate(nums):
-            if nums[1] == True:
-                primes.append(nums[0])
+            if nums[i]**2 > n:
+                break
+            elif nums[i] != 0:
+                for j in range(nums[i]**2,n+1,nums[i]):
+                    if j < len(nums):
+                        nums[j] = 0
+            i += 1
+        for val in nums:
+            if val != 0:
+                primes.append(val)
         return primes
