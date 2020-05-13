29356f743855e59afa9099c5f6e0a26951a8fd3f
Robin-1995 <robinplatts@live.co.uk>
2020-05-13 16:43:23 +0100

    fixed seive of erat bug and added loop to return only prime factors of n

--- prime_factors_of_n.py
+++ prime_factors_of_n.py
@@ -1,5 +1,6 @@
 def prime_factors(n):
 
+    output = []
     def sieve_of_eratosthenes(n):
         nums = [x for x in range(3,n+1) if x%2!=0]
         primes = [2]
@@ -8,11 +9,17 @@ def prime_factors(n):
             if nums[i]**2 > n:
                 break
             elif nums[i] != 0:
-                for j in range(nums[i]**2,n+1,nums[i]):
+                for j in range(nums[i]**2-2-int(nums[i]**2/2),n+1,nums[i]):
                     if j < len(nums):
                         nums[j] = 0
+                    else:
+                        break
             i += 1
         for val in nums:
             if val != 0:
                 primes.append(val)
         return primes
+    for num in sieve_of_eratosthenes(n):
+        if n % num == 0:
+            output.append(num)
+    return output
